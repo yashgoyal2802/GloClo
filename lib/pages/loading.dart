8bac50b3dbd4f83ba890e0aecf06a5e5ad321bb0
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-04-14 22:26:03 +0530

    retriving data

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -13,6 +13,17 @@ class _LoadingState extends State<Loading> {
     Response response =
         await get('http://worldtimeapi.org/api/timezone/Asia/Kolkata');
     Map data = jsonDecode(response.body);
+
+    // get properties from data
+    String datetime = data['datetime'];
+    String offsetH = data['utc_offset'].substring(1, 3);
+    String offsetM = data['utc_offset'].substring(4, 6);
+
+    // creating a datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now
+        .add(Duration(hours: int.parse(offsetH), minutes: int.parse(offsetM)));
+    print(now);
   }
 
   @override
