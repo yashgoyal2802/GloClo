525fb1b0ece01149357e05de55a9844781e739fc
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-04-14 16:57:06 +0530

    routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:gloclo/pages/choose_location.dart';
 import 'package:gloclo/pages/home.dart';
+import 'package:gloclo/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-      home: Home(),
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
     ));
