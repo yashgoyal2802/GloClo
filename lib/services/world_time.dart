7a2ac50b6a11a1c1f7ae0dd149343a1bb5c5c0e8
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-04-14 22:57:39 +0530

    Revert "update 2.0"
    This reverts commit 097308d1a8af0bda74868bcc8c8f0e2820bb64cf.

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -10,27 +10,21 @@ class WorldTime {
   WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async {
-    try {
-      // Make the request
-      Response response =
-          await get('http://worldtimeapi.org/api/timezone/$url');
-      Map data = jsonDecode(response.body);
+    // Make the request
+    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+    Map data = jsonDecode(response.body);
 
-      // get properties from data
-      String datetime = data['datetime'];
-      String offsetH = data['utc_offset'].substring(1, 3);
-      String offsetM = data['utc_offset'].substring(4, 6);
+    // get properties from data
+    String datetime = data['datetime'];
+    String offsetH = data['utc_offset'].substring(1, 3);
+    String offsetM = data['utc_offset'].substring(4, 6);
 
-      // creating a datetime object
-      DateTime now = DateTime.parse(datetime);
-      now = now.add(
-          Duration(hours: int.parse(offsetH), minutes: int.parse(offsetM)));
+    // creating a datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now
+        .add(Duration(hours: int.parse(offsetH), minutes: int.parse(offsetM)));
 
-      // set the time
-      time = now.toString();
-    } catch (e) {
-      print('Caught Error: $e');
-      time = "Could not get time data..";
-    }
+    // set the time
+    time = now.toString();
   }
 }
