1b1c2834230720aa09209c2663902302cc6f2225
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-04-14 22:36:44 +0530

    worldtime class

---
+++ lib/services/world_time.dart
@@ -0,0 +1,32 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+  String location; // Location Name for UI
+  String time; // te time in that location
+  String flag; // url to an flag asset icon
+  String url; // location url for api endpoint
+
+  WorldTime({this.location, this.flag, this.url});
+
+  void getTime() async {
+    // Make the request
+    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+    Map data = jsonDecode(response.body);
+
+    // get properties from data
+    String datetime = data['datetime'];
+    String offsetH = data['utc_offset'].substring(1, 3);
+    String offsetM = data['utc_offset'].substring(4, 6);
+
+    // creating a datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now
+        .add(Duration(hours: int.parse(offsetH), minutes: int.parse(offsetM)));
+
+    // set the time
+    time = now.toString();
+  }
+}
+
+//WorldTime worldtime = WorldTime(location: '', flag: '', url: '');
