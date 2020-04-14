ff4bb078e14fffaa30457fbecc6e2126bfc05951
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-04-14 22:37:09 +0530

    Update loading.dart

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,24 +6,6 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getTime() async {
-    // Make the request
-    Response response =
-        await get('http://worldtimeapi.org/api/timezone/Asia/Kolkata');
-    Map data = jsonDecode(response.body);
-
-    // get properties from data
-    String datetime = data['datetime'];
-    String offsetH = data['utc_offset'].substring(1, 3);
-    String offsetM = data['utc_offset'].substring(4, 6);
-
-    // creating a datetime object
-    DateTime now = DateTime.parse(datetime);
-    now = now
-        .add(Duration(hours: int.parse(offsetH), minutes: int.parse(offsetM)));
-    print(now);
-  }
-
   @override
   void initState() {
     super.initState();
