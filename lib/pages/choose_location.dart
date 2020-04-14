49a000ff8c630f9e941e05eec8b3cef5280e809f
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-04-14 17:37:06 +0530

    http package added

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,6 +6,24 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  void getData() async {
+    // simulate network request for  username
+    await Future.delayed(Duration(seconds: 3), () {
+      print("Yash");
+    });
+
+    // simulate network request to get the bio of the username
+    Future.delayed(Duration(seconds: 2), () {
+      print("painter, coder");
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
