4bd6fb600d79bdb453e63b4cbf18700f77b646fe
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-04-15 17:59:36 +0530

    setting backgroung image

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,44 +13,58 @@ class _HomeState extends State<Home> {
     data2 = ModalRoute.of(context).settings.arguments;
     print(data2);
 
+    // set background
+    String bgTmage =
+        data2['isDaytime'] ? 'day_background.png' : 'night_background.png';
+    Color bgcolor = data2['isDaytime'] ? Colors.blue : Colors.indigo[800];
+
     return Scaffold(
+      backgroundColor: bgcolor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-          child: Column(
-            children: <Widget>[
-              FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text("Edit Location"),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    data2['location'],
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                data2['time'],
-                style: TextStyle(
-                  fontSize: 66.0,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgTmage'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              children: <Widget>[
+                FlatButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(Icons.edit_location),
+                  label: Text("Edit Location"),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data2['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  data2['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
